71d2e8014b5d4cae7535a32c1d3a077ba1bdd51e
koreoxy <rayssankn@gmail.com>
2024-04-04 02:28:24 +0700

    fix : modified blog post list to ordered by date

--- components/app/posts/post-list-page.tsx
+++ components/app/posts/post-list-page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PostConnectionQuery } from "@/tina/__generated__/types";
+import moment from "moment";
 import Link from "next/link";
 import { useTina } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
@@ -11,20 +12,32 @@ export function PostListPageComponent(props: {
   query: string;
 }) {
   const { data } = useTina(props);
-
   const postList = data.postConnection.edges;
+
+  postList?.sort((a: any, b: any) => {
+    const dateA: any = new Date(a.node.date);
+    const dateB: any = new Date(b.node.date);
+    return dateB - dateA;
+  });
 
   return (
     <>
       <h1>Articles</h1>
       <div>
-        {postList?.map((post: any) => (
-          <div key={post.node.id}>
-            <Link href={`/posts/${post.node._sys.filename}`}>
-              {post.node.title}
-            </Link>
-          </div>
-        ))}
+        <ul className="m-0 pl-0">
+          {postList?.map((post: any) => (
+            <li key={post.node.id} className="mt-0 pb-2">
+              <div className="flex">
+                <span className="text-sm text-gray-400">
+                  {moment(post.node.date).format("MMM DD, YYYY")}
+                </span>
+              </div>
+              <Link href={`/posts/${post.node._sys.filename}`}>
+                {post.node.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
